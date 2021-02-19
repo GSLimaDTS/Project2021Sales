3103c0a8771bd801b3bd3215b3d91ea9166c2891
GSLimaDTS <gslima.dts@gmail.com>
2021-02-19 10:23:53 -0300

    My SQL Adaptation and First Migration

--- Project2021Sales/Startup.cs
+++ Project2021Sales/Startup.cs
@@ -37,7 +37,8 @@ namespace Project2021Sales
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<Project2021SalesContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("Project2021SalesContext")));
+                    options.UseMySql(Configuration.GetConnectionString("Project2021SalesContext"), builder =>
+builder.MigrationsAssembly("Project2021Sales")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
