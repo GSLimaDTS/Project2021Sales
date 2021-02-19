2f6350320c535df5f26e5647b0550f68859e0cea
GSLimaDTS <gslima.dts@gmail.com>
2021-02-19 18:40:34 -0300

    Seller details and eager loading

--- Project2021Sales/Controllers/SellersController.cs
+++ Project2021Sales/Controllers/SellersController.cs
@@ -63,5 +63,21 @@ namespace Project2021Sales.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindByID(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
