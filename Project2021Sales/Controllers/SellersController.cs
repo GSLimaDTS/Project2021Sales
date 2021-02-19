b85692d4c7d8a9c2eaf87b586c4724348e962d8e
GSLimaDTS <gslima.dts@gmail.com>
2021-02-19 17:12:57 -0300

    Simple Create Form

--- Project2021Sales/Controllers/SellersController.cs
+++ Project2021Sales/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project2021Sales.Models;
 using Project2021Sales.Services;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,18 @@ namespace Project2021Sales.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
