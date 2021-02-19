bea43264baa56acf870ed889b58c44f13c3fb22e
GSLimaDTS <gslima.dts@gmail.com>
2021-02-18 21:34:23 -0300

    Added CRUD Scaffolding on Departments

---
+++ Project2021Sales/Data/Project2021SalesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project2021Sales.Models;
+
+namespace Project2021Sales.Data
+{
+    public class Project2021SalesContext : DbContext
+    {
+        public Project2021SalesContext (DbContextOptions<Project2021SalesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project2021Sales.Models.Department> Department { get; set; }
+    }
+}
--- Project2021Sales/Startup.cs
+++ Project2021Sales/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Project2021Sales.Data;
 
 namespace Project2021Sales
 {
@@ -33,6 +35,9 @@ namespace Project2021Sales
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<Project2021SalesContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("Project2021SalesContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
